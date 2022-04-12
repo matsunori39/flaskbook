740b0aabb14f5c7646b48f57bb316cbdb592a246
Noriyuki Matsumoto <matsunori39@gmail.com>
2022-04-12 14:58:37 +0900

    Named Endpoints

---
+++ apps/minimalapp/app.py
@@ -0,0 +1,16 @@
+# Flaskクラスをimportする
+from flask import Flask
+
+# Flaskクラスをインスタンス化する
+app = Flask(__name__)
+
+
+# URLと実行する関数をマッピングする
+@app.route("/")
+def index():
+    return "Hello, Flaskbook!"
+
+
+@app.route("/hello", methods=["GET"], endpoint="hello-endpoint")
+def hello():
+    return "Hello, World!"
