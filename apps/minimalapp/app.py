f468e9fe47b2cce5ddbc7925dc066715e3845ffd
Noriyuki Matsumoto <matsunori39@gmail.com>
2022-04-13 14:48:07 +0900

    Created minimal apps

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,5 +1,5 @@
 # Flaskクラスをimportする
-from flask import Flask
+from flask import Flask, render_template, url_for, current_app, g, request
 
 # Flaskクラスをインスタンス化する
 app = Flask(__name__)
@@ -11,6 +11,42 @@ def index():
     return "Hello, Flaskbook!"
 
 
-@app.route("/hello", methods=["GET"], endpoint="hello-endpoint")
-def hello():
-    return "Hello, World!"
+@app.route("/hello/<name>", methods=["GET", "POST"], endpoint="hello-endpoint")
+def hello(name):
+    # Python 3.6から導入されたf-stringで文字列を定義
+    return f"Hello, {name}!"
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    # 変数をテンプレートエンジンに渡す
+    return render_template("index.html", name=name)
+
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/ichiro?page=1
+    print(url_for("show_name", name="ichiro", page="1"))
+
+# ここで呼び出すとエラーになる
+# print(current_app)
+
+# アプリケーションコンテキストを取得してスタックへpushする
+ctx = app.app_context()
+ctx.push()
+
+# current_appにアクセスが可能になる
+print(current_app.name)
+# >> apps.minimalapp.app
+
+# グローバルなテンポラリ領域に値を設定する
+g.connection = "connection"
+print(g.connection)
+# >> connection
+
+with app.test_request_context("/users?updated=true"):
+    # trueが出力される
+    print(request.args.get("updated"))
