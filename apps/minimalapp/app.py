6c04ba88901ad3236364884e45f9ac0cc4b945fb
Noriyuki Matsumoto <matsunori39@gmail.com>
2022-04-14 13:40:10 +0900

    Add Flash message

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,8 +1,20 @@
-# Flaskクラスをimportする
-from flask import Flask, current_app, g, render_template, request, url_for, redirect
+from email_validator import validate_email, EmailNotValidError
+# import文が長くなるため改行
+from flask import (
+    Flask,
+    current_app,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for,
+    flash,
+)
 
 # Flaskクラスをインスタンス化する
 app = Flask(__name__)
+# SECRET_KEYを追加する
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 
 
 # URLと実行する関数をマッピングする
@@ -60,14 +72,38 @@ def contact():
 @app.route("/contact/complete", methods=["GET", "POST"])
 def contact_complete():
     if request.method == "POST":
-        # form属性を使ってフォームの値を取得する
         username = request.form["username"]
         email = request.form["email"]
         description = request.form["description"]
 
+        # 入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力して下さい")
+            is_valid = False
+
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+
+        if not is_valid:
+            return redirect(url_for("contact"))
+
         # メールを送る(最後に実装)
 
-        # contactエンドポイントへリダイレクトする
+        # 問い合わせ完了エンドポイントへリダイレクトする
+        flash("問い合わせ内容はメールにて送信しました。問い合わせありがとうございます。")
         return redirect(url_for("contact_complete"))
 
     return render_template("contact_complete.html")
