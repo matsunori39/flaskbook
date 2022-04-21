5bba4c9585c6f41910d8cf0a91ce5c4b99f2b98a
Noriyuki Matsumoto <matsunori39@gmail.com>
2022-04-21 16:56:32 +0900

    Set SQL log output

--- apps/app.py
+++ apps/app.py
@@ -15,9 +15,11 @@ def create_app():
     # アプリのコンフィグ設定をする
     app.config.from_mapping(
         SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI=
-            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
+        SQLALCHEMY_DATABASE_URI="sqlite:////" +
+        str(Path(Path(__file__).parent.parent, "local.sqlite")),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        # SQLをコンソールログに出力する設定
+        SQLALCHEMY_ECHO=True
     )
 
     # SQLAlchemyとアプリを連携する
