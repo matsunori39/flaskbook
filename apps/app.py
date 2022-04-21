db81b257ce0e765fd05572b44568c2acd0b5e097
Noriyuki Matsumoto <matsunori39@gmail.com>
2022-04-21 14:49:22 +0900

    flask db init

--- apps/app.py
+++ apps/app.py
@@ -1,10 +1,29 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+# SQLAlchemyをインスタンス化する
+db = SQLAlchemy()
 
 
 # create_app関数を作成する
 def create_app():
     # Flaskインスタンスを生成
     app = Flask(__name__)
+    # アプリのコンフィグ設定をする
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI=
+            f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    # SQLAlchemyとアプリを連携する
+    db.init_app(app)
+    # Migrateとアプリを連携する
+    Migrate(app, db)
 
     # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
